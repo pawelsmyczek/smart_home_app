cda1342d7e8b9f14613583876000f43f13bc1688
Pawel Smyczek <grandefinale@pawel.pl>
2020-01-04 15:41:54 +0100

    sample

--- app/src/main/java/com/example/pablito/first_one/graphView.java
+++ app/src/main/java/com/example/pablito/first_one/graphView.java
@@ -56,7 +56,7 @@ public class graphView extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG )
-                        .setAction( "Action", null ).show();
+                        .setAction( "ActionButton", null ).show();
             }
         } );*/
     }
